8c80bb8fd4d441115a12b82355c80d38e495f9dd
yk <yefim_korshever@mail.ru>
2022-12-03 18:41:32 +0300

    Get groups

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 
 import os
+import pprint
 from pathlib import Path
 from urllib.parse import unquote, urlsplit
 
 import requests
+from environs import Env
 
 
 def get_link_extension(link):
@@ -23,7 +25,7 @@ def download_image(url, folder_name, file_name, payload=None):
         file.write(response.content)
 
 
-def main():
+def get_comic():
     comic_number = 353
     url = f'https://xkcd.com/{comic_number}/info.0.json'
     connect_timeout = 3.05
@@ -37,6 +39,35 @@ def main():
     download_image(img_link, folder_name, f'comic_{comic_number}{extension}')
     comment = comic_card['alt']
     print(comment)
+
+
+def get_groups(access_token, vk_api_version):
+    url = 'https://api.vk.com/method/groups.get'
+    payload = {
+        'access_token': access_token,
+        'v': vk_api_version,
+        'extended': 1,
+    }
+    connect_timeout = 3.05
+    read_timeout = 27
+    response = requests.get(
+        url,
+        params=payload,
+        timeout=(connect_timeout, read_timeout)
+    )
+    response.raise_for_status()
+    groups = response.json()
+    pprint.pprint(groups)
+
+
+def main():
+
+    env = Env()
+    env.read_env()
+    access_token = env('ACCESS_TOKEN')
+    # get_comic()
+    vk_api_version = 5.131
+    get_groups(access_token, vk_api_version)
 
 
 if __name__ == '__main__':
